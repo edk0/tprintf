60159770df5ec34609159b51e21b7472ff541e16
Ed Kellett <edk141@gmail.com>
2014-11-28 22:44:42 +0000

    Add stuff to main()

--- main.c
+++ main.c
@@ -1,4 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+
 #include "tstdio.h"
+#include "tstd.h"
+#include "tprintf.h"
+
+static tpf_conv conv_r;
+static int conv_r(struct tpf_state *state, va_list *ap)
+{
+	int i;
+	char *hex = "0123456789ABCDEF";
+	unsigned char *p = va_arg(*ap, void *);
+
+	if (!state->prec_set)
+		return -1;
+
+	for (i = 0; i < state->prec; i++) {
+		if (strchr(state->flags, '!') && i > 0 && i % 2 == 0)
+			tpf_write(state, 1, " ");
+		tpf_write(state, 1, hex + (p[i] >> 4 & 0xF));
+		tpf_write(state, 1, hex + (p[i]      & 0xF));
+	}
+
+	return 0;
+}
 
 int main(void)
 {
@@ -6,9 +31,22 @@ int main(void)
 	char b[32];
 	tprintf__init();
 
+	tpf_register(tprintf__context, 'r', "!", conv_r);
+
 	n = tprintf_snprintf(b, sizeof b, "This is a long string that should be cut off at 31 characters.");
 
 	tprintf_printf("%p is %zu characters long: \"%s\"\n", (void *)b, n, b);
+
+	tprintf_printf("octal constants: %o %#o\n", 0777, 0777);
+
+	tprintf_printf("object representation:\n%!.*r\n", (int) sizeof b, (void *) b);
+
+	tprintf_printf("integers: %010.5d\n", -59);
+	printf("integers: %010.5d\n", -59);
+
+	tprintf_printf("%-20s (%-5i|%5i)\n", "Justification.", 17, 17);
+	tprintf_printf("0        1         2\n");
+	tprintf_printf("12345678901234567890\n");
 
 	return 0;
 }
